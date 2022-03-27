36083f07eaf07d9ada60363e8cdb2b9fc2fcb016
PAWAN KUMAR <97441423+Pawan8271@users.noreply.github.com>
2022-03-27 23:34:45 +0530

    Add files via upload

---
+++ Queue/Queue1.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+#define n 20
+
+class queue{
+	int* arr;
+	int front;
+	int back;
+	
+	public:
+		queue(){
+			arr = new int[n];
+			front = -1;
+			back = -1;
+		}
+	void push(int x){
+		if(back==n-1){
+			cout<<"Queue overflow"<<endl;
+			return;
+		}
+		back++;
+		arr[back]=x;
+		
+		if(front=-1){
+			front++;
+		}
+	}
+	void pop(){
+		if(front==-1 || front>back){
+			cout<<"No element for pop ";
+			return;
+		}
+		front++;
+	}
+	int peek(){
+			if(front==-1 || front>back){
+			cout<<"No element for pop ";
+			return -1;
+		}
+		return arr[front];
+	}
+	bool empty(){
+	if(front==-1 || front>back){
+			return true;	
+    	}
+	return false;
+   }
+};
+
+int main(){
+	
+	queue q;
+	q.push(1);
+	q.push(2);
+	q.push(3);
+	
+	cout<<q.peek()<<endl;
+	q.pop();
+	cout<<q.peek()<<endl;
+	q.pop();
+	cout<<q.peek()<<endl;
+	q.pop();
+	
+	cout<<q.empty()<<endl;
+	
+	getch();
+	return 0;
+}
